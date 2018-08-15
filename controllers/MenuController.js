1b755d25c2578bc74d12ba42e4b1f6470734dcd6
David Probst <probst.dj2@gmail.com>
2018-08-14 19:53:42 -0500

    add assignment 1 code

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,6 +9,7 @@ module.exports = class MenuController {
           message: "Please choose from an option below:",
           choices: [
             "Add new contact",
+            "Get the date",
             "Exit"
           ]
       }
@@ -22,6 +23,9 @@ module.exports = class MenuController {
       switch(response.mainMenuChoice){
         case "Add new contact":
           this.addContact();
+          break;
+        case "Get the date":
+          this.getDate();
           break;
         case "Exit":
           this.exit();
@@ -42,6 +46,14 @@ module.exports = class MenuController {
   addContact(){
     this.clear();
     console.log('addContact called');
+    this.main();
+  }
+
+  getDate(){
+    this.clear();
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    var date = new Date();
+    console.log(date.toLocaleDateString("en-US", options));
     this.main();
   }
 
