14cc54fa008cf38e1e97be03b68a83edd728f252
David Probst <probst.dj2@gmail.com>
2018-08-16 18:51:24 -0500

    testing assignment

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -5,6 +5,7 @@ describe("MenuController", () => {
   beforeEach(() => {
     this.menu = new MenuController();
   });
+
   describe("#getContactCount()", () => {
    it("should return 0 when no contacts are in the book", () => {
       expect(this.menu.getContactCount()).toBe(0);
@@ -13,5 +14,10 @@ describe("MenuController", () => {
      this.menu.contacts.push("Bob");
      expect(this.menu.getContactCount()).toBe(1)
    });
- });
+  });
+  describe("#remindMe", () => {
+    it("should return 'Learning is a life-long pursuit'", () => {
+      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+    })
+  })
 });
