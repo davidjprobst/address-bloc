e31c8a245584daca0f5d2ed14552e8f78246a29d
David Probst <probst.dj2@gmail.com>
2018-08-17 17:30:36 -0500

    database connect checkpoint work

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,4 +1,5 @@
 const MenuController = require("../controllers/MenuController");
+const Contact = require("../db/models").Contact;
 
 describe("MenuController", () => {
 
@@ -6,18 +7,4 @@ describe("MenuController", () => {
     this.menu = new MenuController();
   });
 
-  describe("#getContactCount()", () => {
-   it("should return 0 when no contacts are in the book", () => {
-      expect(this.menu.getContactCount()).toBe(0);
-   });
-   it("should return 1 when there is exactly one contact in the book", () => {
-     this.menu.contacts.push("Bob");
-     expect(this.menu.getContactCount()).toBe(1)
-   });
-  });
-  describe("#remindMe", () => {
-    it("should return 'Learning is a life-long pursuit'", () => {
-      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
-    })
-  })
 });
